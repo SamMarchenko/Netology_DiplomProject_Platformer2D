44cd055269f56e78ad9f491595955326c43d2dd5
Sam Marchenko <sam.marchenko@gmail.com>
2023-03-13 15:08:43 +0300

    Вернул логику инъекции player. Отключил фабрики

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,31 +6,20 @@ namespace DefaultNamespace.Player
     public class Player
     {
         private PlayerView _playerView;
-        private readonly SpawnPositions _spawnPositions;
-        private readonly PlayerViewFactory _factory;
         private readonly InputService _input;
         private PlayerController _controller;
 
-        public Player(InputService input, PlayerView playerView,
-            SpawnPositions spawnPositions, PlayerViewFactory factory)
+        public Player(InputService input, PlayerView playerView, PlayerController controller)
         {
             _input = input;
-            
+            _controller = controller;
             _playerView = playerView;
-            _spawnPositions = spawnPositions;
-            _factory = factory;
-
             Subscribe();
-            CreatePlayerView(spawnPositions);
-            _controller = new PlayerController(_playerView);
+          
+
             //todo: где отписываться не в монобехе?
         }
-
-        private void CreatePlayerView(SpawnPositions spawnPositions)
-        {
-          _playerView = _factory.CreatePlayer(_playerView, spawnPositions.PlayerSpawnPos);
-        }
-
+        
         private void Subscribe()
         {
             _input.OnMove += OnMove;
