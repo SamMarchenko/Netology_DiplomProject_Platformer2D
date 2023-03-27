f938fe6faba93b50cb8025b51779f98658a6e847
Sam Marchenko <sam.marchenko@gmail.com>
2023-03-27 18:23:50 +0300

    Добавил логику урона от пассивного врага

--- Assets/Scripts/Enemies/PassiveEnemy/PassiveEnemyView.cs
+++ Assets/Scripts/Enemies/PassiveEnemy/PassiveEnemyView.cs
@@ -1,7 +1,15 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+namespace DefaultNamespace
 {
     public class PassiveEnemyView : EnemyView
     {
-        
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Player"))
+            {
+                OnConnectWithPlayer?.Invoke(EUnitType.Enemy);
+            }
+        }
     }
 }
--- Assets/Scripts/Strategy/PassiveEnemyStrategy.cs
+++ Assets/Scripts/Strategy/PassiveEnemyStrategy.cs
@@ -4,14 +4,27 @@ namespace DefaultNamespace.Strategy
 {
     public class PassiveEnemyStrategy : IBehaviourStrategy
     {
+        private PassiveEnemyView _view;
+
         public void PassiveBehaviour(EnemyView enemyView)
         {
-           
+           SetView(enemyView);
         }
 
         public void ActiveBehaviour(EnemyView enemyView)
         {
-           
+            SetView(enemyView);
         }
+
+
+        private void SetView(EnemyView enemyView)
+        {
+            if (_view == null || _view != enemyView)
+            {
+                _view = (PassiveEnemyView) enemyView;
+                
+            }
+        }
+        
     }
 }
